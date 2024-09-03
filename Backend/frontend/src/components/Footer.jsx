8ead3865e708cd4d08bd4fd9205f8ea80ab3bce0
suman <imrancodingschool@gmail.com>
2024-09-03 13:22:27 +0530

    added

--- Backend/frontend/src/components/Footer.jsx
+++ Backend/frontend/src/components/Footer.jsx
@@ -4,21 +4,23 @@ import {
   FaSquareTwitter,
   FaSquareInstagram,
   FaYoutube,
-  FaLinkedin,
+  FaWhatsapp,
 } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <footer>
       <div>
-        <img src="/websitelogo.png" alt="logo" />
+        <img src="\donation10Message-removebg-preview.png" alt="logo" />
       </div>
       <div>
         <h4>Support</h4>
         <ul>
-          <li>New York, USA</li>
-          <li>crypto@gmail.com</li>
-          <li>+92 123123123</li>
+          <li>Baharban B.belahi
+            <p>Madhubani <span style={{color:"blue"}}>Bihar</span></p>
+          </li>
+          <li>teamdonation10@gmail.com</li>
+          <li>814988700</li>
         </ul>
       </div>
       <div>
@@ -42,7 +44,7 @@ const Footer = () => {
         <h4>Follow Us</h4>
         <ul>
           <li>
-            <Link to={"/"}>
+            <Link to={"https://x.com/teamdonation10?t=FAQP_Em0Y-vjaRFbOmJ83A&s=09"}>
               {" "}
               <span>
                 <FaSquareTwitter />
@@ -51,7 +53,7 @@ const Footer = () => {
             </Link>
           </li>
           <li>
-            <Link to={"/"}>
+            <Link to={"https://youtube.com/@TeamDonation10?si=Av-N7xUOGkB2wjHN"}>
               {" "}
               <span>
                 <FaYoutube />
@@ -60,7 +62,7 @@ const Footer = () => {
             </Link>
           </li>
           <li>
-            <Link to={"/"}>
+            <Link to={"https://www.instagram.com/donation10foundation"}>
               {" "}
               <span>
                 <FaSquareInstagram />
@@ -69,12 +71,12 @@ const Footer = () => {
             </Link>
           </li>
           <li>
-            <Link to={"/"}>
+            <Link to={"https://whatsapp.com/channel/0029Vaar3SWKwqSWc4eHQT0p"}>
               {" "}
               <span>
-                <FaLinkedin />
+              <FaWhatsapp />
               </span>{" "}
-              <span>Linkedin</span>{" "}
+              <span>Whatsapp</span>{" "}
             </Link>
           </li>
         </ul>
